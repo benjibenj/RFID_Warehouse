cad3ada73aaf02950d0f587419decbfcc7700fe4
Benjamin Vaysse <benjamin.vaysse@gmail.com>
2019-03-31 23:50:25 +0200

    adapted file names

--- Serial.py
+++ Serial.py
@@ -2,9 +2,9 @@ import serial
 import datetime
 
 ser = serial.Serial("/dev/ttyUSB0", 115200)
-# Connect with Raspberry Pi
+# Connect to Raspberry Pi serial port
 
-with open("RFIDtrace.txt", "ab") as f:
+with open("files/Detected_products.txt", "ab") as f:
     for x in range(10):
         # get tag id thought serial port
         line = ser.readline().decode("utf-8")
